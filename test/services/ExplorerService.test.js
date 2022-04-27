21a6f6c6ffcc4470f498e791c855a46032fde9c9
Harold Barron <haroldbarron13@gmail.com>
2022-04-27 18:38:28 -0500

    Creating test to get the amount of explorers by their mission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -6,6 +6,11 @@ describe("Unit test for Explorer Services", () =>
     {
         const filterByNode = ExplorerService.filterByMission("explorers.json","node")
         const ExplorersWithNode = filterByNode.forEach(expName => expect(expName.mission).toContain("node"))
+    })
+    test("2. Get the amount of explorers by their mission", () =>
+    {
+        const ExplorerByMission = ExplorerService.getAmountOfExplorersByMission("explorers.json","node")
+        expect(ExplorerByMission).toBe(10)
         
     })
 })
